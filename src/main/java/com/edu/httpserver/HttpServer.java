ba2a0ff0a6c0642169161acd83a3dcc81c5a3319
JuanC-358 <juanc358a@gmail.com>
2021-09-01 16:05:13 -0500

    ServidorWEB 4PM2

--- src/main/java/com/edu/httpserver/HttpServer.java
+++ src/main/java/com/edu/httpserver/HttpServer.java
@@ -10,74 +10,79 @@ package com.edu.httpserver;
  * @author ADMIN
  */
 import java.net.*;
- import java.io.*;
- public class HttpServer {
- public static void main(String[] args) throws IOException {
- ServerSocket serverSocket = null;
-try {
- serverSocket = new ServerSocket(35000);
-} catch (IOException e) {
- System.err.println("Could not listen on port: 35000.");
-System.exit(1);
- }
- Socket clientSocket = null;
-try {
- System.out.println("Listo para recibir ...");
-clientSocket = serverSocket.accept();
- } catch (IOException e) {
-System.err.println("Accept failed.");
- System.exit(1);
-}
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
- BufferedReader in = new BufferedReader(
-new InputStreamReader(
- clientSocket.getInputStream()));
-String inputLine, outputLine;
+public class HttpServer {
 
-while ((inputLine = in.readLine()) != null) {
- System.out.println("Received: " + inputLine);
-if (!in.ready()) {
- break;
-}
- }
-outputLine = "<!DOCTYPE html>"
- + "<html>"
-+ "<head>"
- + "<meta charset=\"UTF-8\">"
-+ "<title>Title of the document</title>\n"
-+ "</head>"
-+ "<body>"
- + "My Web Site"
-+ "</body>"
- + "</html>" + inputLine;
-out.println(outputLine);
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        Socket clientSocket = null;
+        try {
+            System.out.println("Listo para recibir ...");
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
 
-out.close();
- in.close();
-clientSocket.close();
- serverSocket.close();
-}
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        clientSocket.getInputStream()));
+        String inputLine, outputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Received: " + inputLine);
+            if (!in.ready()) {
+                break;
+            }
+        }
+        outputLine = "<!DOCTYPE html>"
+                + "<html>"
+                + "<head>"
+                + "<meta charset=\"UTF-8\">"
+                + "<title>Title of the document</title>\n"
+                + "</head>"
+                + "<body>"
+                + "My Web Site"
+                + "</body>"
+                + "</html>" + inputLine;
+        out.println(outputLine);
+
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
 
     public static Object getInstance() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    private String getComonentResurce(URI resourceURI){
-    String response = default404HTMLResponse();
-    try{
-        String classPath = resourceURI.getPath().toString().replaceAll("/appuser/","");
-        Class component = Class.forName(classPath);
-        for(Method m : component.getDeclareMethods()){
-            if(m.isAnnotationPresent(Service.class)){
-                response = m.invoke(null).toString();
-                response = "HTTP/1.1 200 ok\r\n"
-                        +"Content-Type: text/html\r\n"
-                        +"\r\n" + response;
+
+    private String getComonentResurce(URI resourceURI) {
+        String response = default404HTMLResponse();
+        try {
+            String classPath = resourceURI.getPath().toString().replaceAll("/appuser/", "");
+            Class component = Class.forName(classPath);
+            for (Method m : component.getDeclareMethods()) {
+                if (m.isAnnotationPresent(Service.class)) {
+                    response = m.invoke(null).toString();
+                    response = "HTTP/1.1 200 ok\r\n"
+                            + "Content-Type: text/html\r\n"
+                            + "\r\n" + response;
+                }
             }
+        } catch (Exception ex) {
+            Logger.getLogger(HttpServer.class.getName()).Log(Level.SEVERE, null, ex);
         }
-    }catch(Exception ex)
-        Logger.getLogger(HttpServer.class.getName())Log(Level.SEVERE, null,ex);
-    {
     }
-    }
- }
+}
+
